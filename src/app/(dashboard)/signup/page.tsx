8e725917db76dcb54bb04ebc15588e0a5d3bac48
unknown <abdullah22shiban42@gmail.com>
2023-11-06 13:27:48 +0300

    add basic auth pages

--- src/app/(dashboard)/signup/page.tsx
+++ src/app/(dashboard)/signup/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 
 export default function page() {
-   
+    "use client"
  const token = localStorage.getItem("token");
  if(token) redirect("dashboard");
   return (
