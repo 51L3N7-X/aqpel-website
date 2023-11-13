967e04b79ab57858d79008718c817a0c6b6c92ff
51L3N7 <66969240+51L3N7-X@users.noreply.github.com>
2023-11-13 10:30:03 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
   test: true,
-=======
   images: {
     domains: ["toppng.com"],
   },
   experimental: {
     serverActions: true,
   },
->>>>>>> refs/remotes/origin/main
 };
 
 module.exports = nextConfig
